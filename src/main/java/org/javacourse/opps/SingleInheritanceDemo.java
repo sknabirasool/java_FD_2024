497c0ee3efcc1b74a72be6da4fb60cf2352111e2
sknabirasool <sknabirasool104@gmail.com>
2024-01-28 08:03:53 +0530

    save

--- src/main/java/org/javacourse/opps/SingleInheritanceDemo.java
+++ src/main/java/org/javacourse/opps/SingleInheritanceDemo.java
@@ -1,2 +1,30 @@
-package org.javacourse.opps;public class SingleInheritanceDemo {
+package org.javacourse.opps;
+
+
+
+class Father{
+    String color="white";
+    public void foodie()
+    {
+        System.out.println("Father is super foodie");
+    }
+}
+class  Child extends Father{
+    int height=6;
+    public  void games()
+    {
+        System.out.println("child is intrested games");
+    }
+}
+public class SingleInheritanceDemo {
+
+    public static void main(String[] args) {
+
+        Child c=new Child();
+
+        System.out.println(" Father color is "  +c.color);
+        c.foodie();
+        System.out.println("Child height "+c.height);
+        c.games();
+    }
 }
